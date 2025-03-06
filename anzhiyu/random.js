2b14f0e76e38a26ed252f6294df179d6e83a309a
2lnz <jidedaren@163.com>
2025-03-06 20:42:56 +0800

    Site updated: 2025-03-06 20:42:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/第一篇博客/","2025/02/07/hello-world/"];function toRandomPost(){
+var posts=["2025/02/07/第一篇博客/","2025/02/07/hello-world/","2025/03/06/大学老师-究竟在干嘛？/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
