8db447d2315a4d08aab6fb3baba2e49f4fc997fb
23blocks <jidedaren@163.com>
2025-07-14 21:27:58 +0800

    Site updated: 2025-07-14 21:27:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/第一篇博客/","2025/02/07/hello-world/","2025/03/06/大学老师-究竟在干嘛？/"];function toRandomPost(){
+var posts=["2025/02/07/hello-world/","2025/03/06/大学老师-究竟在干嘛？/","2025/02/07/第一篇博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
