7ffb00646a4a4ca394bf22e7ce686300a54cb9b8
23blocks <jidedaren@163.com>
2025-10-30 17:06:56 +0800

    Site updated: 2025-10-30 17:06:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/07/hello-world/","2025/03/06/大学老师-究竟在干嘛？/","2025/02/07/第一篇博客/"];function toRandomPost(){
+var posts=["2025/02/07/hello-world/","2025/02/07/第一篇博客/","2025/03/06/大学老师-究竟在干嘛？/","2025/08/21/黄杨钿甜事件我的看法/","2025/10/25/教育资源分布不公导致的问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
